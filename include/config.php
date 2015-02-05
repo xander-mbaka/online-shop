ea7ebba3714b49b24a6662bfd2e9579eebed0abc
Alex Mbaka <mbakalex@gmail.com>
2015-02-05 12:03:56 +0300

    commit update include

--- include/config.php
+++ include/config.php
@@ -21,22 +21,22 @@
 	ini_set('sendmail_from', SENDMAIL_FROM);
 	// By default we don't log errors to a file
 	define('LOG_ERRORS', false);
-	define('LOG_ERRORS_FILE', 'C:\\xampp\\htdocs\\lightning\\errors_log.txt'); // Windows
+	define('LOG_ERRORS_FILE', 'C:\\xampp\\htdocs\\shopping\\errors_log.txt'); // Windows
 	// define('LOG_ERRORS_FILE', '/home/username/tshirtshop/errors.log'); // Linux
 	/* Generic error message to be displayed instead of debug info
 	(when DEBUGGING is false) */
-	define('SITE_GENERIC_ERROR_MESSAGE', '<h1>Lightning Abstracts Error!</h1>');
+	define('SITE_GENERIC_ERROR_MESSAGE', '<h1>shopping Abstracts Error!</h1>');
 	// Database connectivity setup
 	define('DB_PERSISTENCY', 'true');
 	define('DB_SERVER', 'localhost');
 	define('DB_USERNAME', 'admin');
-	define('DB_PASSWORD', 'andromedapx660');
-	define('DB_DATABASE', 'lightning');
+	define('DB_PASSWORD', 'xxxxxxxx');
+	define('DB_DATABASE', 'shopping');
 	define('PDO_DSN', 'mysql:host=' . DB_SERVER . ';dbname=' . DB_DATABASE);
 	// Server HTTP port (can omit if the default 80 is used)
 	define('HTTP_SERVER_PORT', '80');
 	/* Name of the virtual directory the site runs in, for example:
 	'/tshirtshop/' if the site runs at http://www.example.com/tshirtshop/
 	'/' if the site runs at http://www.example.com/ */
-	define('VIRTUAL_LOCATION', '/lightning/');
+	define('VIRTUAL_LOCATION', '/shopping/');
 ?>
